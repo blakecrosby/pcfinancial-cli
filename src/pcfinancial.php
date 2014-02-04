58ea069fe0c7ba55034c4c73aea56acb43edda47
Blake Crosby <me@blakecrosby.com>
2014-02-04 17:29:52 -0500

    got basic functionality working for getting account balances

--- src/pcfinancial.php
+++ src/pcfinancial.php
@@ -1,34 +1,61 @@
 <?php
 #!/usr/bin/php
 
+
+
 # Set some stuff up
 $posturl = "https://m.pcfinancial.ca/middleware/MWServlet";
+
+#set the default timezone to avoid php Date warnings
+date_default_timezone_set('EST');
 
 # Command line options
 # c: card number
 # p: password
-$options = getopt("c:p:");
+$options = getopt("c:p:a:");
+
 
 if (!$options) {
     print "pcfinancial-cli: Command Line Options\n";
     print "Required:\n";
     print "\t-c Card Number\n\t-p Web Banking Password\n\n";
+    print "By default your account balances are shown, however you can alter this behaviour with the following:\n";
+    print "\t -a <account number> Display details for specific account number\n\n";
+    die;
 }
 
+# Get session cookie and login
+get('checkAppNContentUpgrade',array('contentTimestamp' => time()*1000));
+get('signOn',array('password'=>$options['p'],'cardNumber'=>$options['c'],'packageName'=>'SystemAccess','contentTimestamp' => time()*1000));
 
-    # Get session cookie and login
-    get('checkAppNContentUpgrade');
-    get('signOn',array('password'=>$options['p'],'cardNumber'=>$options['c'],'packageName'=>'SystemAccess'));
+# Get account information
+$accountinfo = get('accountSummary',array('channelId' => 'mobile','cacheid' => '','packageName'=>'Accounts','timestamp' => date("Ynj".intval(date('G').intval(date('i').intval(date('s')))))));
+$accountinfo = json_decode($accountinfo);
 
-    #Get Account balances
-    #get('')
+# Default behaviour. Show accounts and balances.
+foreach ($accountinfo->accountsList as $account) {
+
+    # If is a single account (-a) was chosen, only display it.
+    if (isset($options['a'])) {
+        if ($account->accountNumber == $options['a']) {
+            print "$account->productName ($account->accountNumber) - Available: $account->availableBalance Current: $account->currentBalance\n";
+            break;
+        }
+    }
+    else {
+        print "$account->productName ($account->accountNumber) - Available: $account->availableBalance Current: $account->currentBalance\n";
+    }
+}
+
+#Log out
+get('signOff');
+
 
 function get($service,$parameters = Array()) {
     global $posturl;
 
     $fields = array(
         'platform' => 'android',
-        'contentTimestamp' => time()*1000,
         'platformId' => 'android',
         'appID' => 'PCFMobile',
         'appver' => '1.3',
@@ -39,12 +66,16 @@ function get($service,$parameters = Array()) {
     );
     $fields = array_merge($fields,$parameters);
 
-    print http_build_query($fields);
     $ch = curl_init($posturl);
     curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
     curl_setopt($ch,CURLOPT_POSTFIELDS, http_build_query($fields));
     curl_setopt($ch, CURLOPT_COOKIEJAR, "/tmp/cookieFileName");
+    curl_setopt($ch, CURLOPT_COOKIEFILE, "/tmp/cookieFileName");
+    curl_setopt($ch,CURLOPT_USERAGENT,'Apache-HttpClient/android/Nexus 5');
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Cookie2: $Version=1'));
     $output = curl_exec($ch);
-    print $output;
+
+    return $output;
 
 }
